4bbe3bb2d8d7e5f8862998992225bc8c70891ad1
vidhyasri123 <vidhyashok1110@gmail.com>
2025-04-03 03:01:57 -0700

    Add files via upload

---
+++ tvm/ram/Scripts.js
@@ -0,0 +1,74 @@
+// variable :
+
+/*
+
+let
+var
+const
+*/
+
+// operators 
+
+// arithmatic operators
+
+
+
+
+// let a = 10
+// let b = 2
+// var c = a+b
+// console.log(c)
+// console.log(a/b)
+// console.log(a*b)
+// console.log(a**b)
+
+
+// assignment operators 
+
+// var x =10 
+// var y = 11
+// console.log(x)
+
+
+
+// x /=  5
+// console.log(x)
+
+
+// comparison operator : 
+
+
+/*
+
+
+<
+>
+<=
+>=
+
+*/
+
+
+// function 
+
+
+function work(a){
+    console.log(a)
+
+
+
+}
+
+
+work('arun')
+
+
+
+
+
+
+
+
+
+
+
